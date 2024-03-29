dcc6defd2616d875ecdca913c915660c3d8cf270
ajadreddy30611 <ajadreddy.edavelly@azistaindustries.com>
2024-03-29 11:05:20 +0530

    uploaded tiff image to openlayers using geoserver

--- main.js
+++ main.js
@@ -3,13 +3,15 @@ import Map from 'ol/Map.js';
 import OSM from 'ol/source/OSM.js';
 import View from 'ol/View.js';
 import {Image as ImageLayer, Tile as TileLayer} from 'ol/layer.js';
+import {useGeographic} from 'ol/proj.js';
 
+useGeographic();
 const layers = [
   new TileLayer({
     source: new OSM(),
   }),
   new ImageLayer({
-    // extent: [-13884991, 2870341, -7455066, 6338219],
+    extent: [79.8464062869999935,37.0293226134035152, 79.8809415705140253,37.0477340179999999],
     source: new ImageWMS({
       url: 'http://localhost:8080/geoserver/Tiff_Layers/wms',
       params: {'LAYERS': 'Tiff_Layers:Images_airport'},
@@ -22,7 +24,8 @@ const map = new Map({
   layers: layers,
   target: 'map',
   view: new View({
-    center: [-10997148, 4569099],
-    zoom: 4,
+    center: [79.86367392875701,37.038528315701754],
+    // center: ol.proj.transform([37.038528315701754, 79.86367392875701],'EPSG:4326', 'EPSG:3857'),
+    zoom: 14,
   }),
 });
